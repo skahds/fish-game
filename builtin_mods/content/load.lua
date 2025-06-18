05ab27385dd20825dd4f1ee72a1dc314de11dbb2
skahds <skahd.dev@gmail.com>
2025-06-18 22:02:14 +0700

    change background color so it's cooler

--- builtin_mods/content/load.lua
+++ builtin_mods/content/load.lua
@@ -1,7 +1,10 @@
+love.graphics.setBackgroundColor(0.4, 0.5, 0.7)
+
 nk.on("@load", function ()
     nk.main.trySpawnIngredient("chicken_meat", nk.main.posToGridPos(1, 1, nk.main.grid.info.ingredientZ))
     nk.main.trySpawnIngredient("vegetable", nk.main.posToGridPos(2, 2, nk.main.grid.info.ingredientZ))
     nk.main.trySpawnIngredient("vegetable", nk.main.posToGridPos(3, 2, nk.main.grid.info.ingredientZ))
 
-    nk.main.trySpawnBuilding("stove", nk.main.posToGridPos(3, 2, nk.main.grid.info.buildingZ))
+    nk.main.trySpawnBuilding("stove", nk.main.posToGridPos(3, 3, nk.main.grid.info.buildingZ))
+    nk.main.trySpawnBuilding("stove", nk.main.posToGridPos(4, 5, nk.main.grid.info.buildingZ))
 end)

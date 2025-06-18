14004921d242f0370b5c6f56eaf19ef080588d01
skahds <skahd.dev@gmail.com>
2025-06-18 18:42:39 +0700

    ui class rework

--- builtin_mods/ui/export.lua
+++ builtin_mods/ui/export.lua
@@ -4,9 +4,28 @@ ui.add(ui), does table.insert UI, so the order goes on top
 ]]
 
 nk.ui = {}
+nk.ui.uiClasses = {}
 nk.ui.world = {}
 
-function nk.ui.addUI(ui)
-    table.insert(nk.ui.world, ui)
+function nk.ui.spawnUI(id, ret)
+    ret = ret or false
+
+    table.insert(nk.ui.world, nk.ui.uiClasses[id]:new())
+    print("createUI " .. id)
 end
 
+--[[
+function nk.main.spawnEntity(id, args, ret)
+    ret = ret or false
+
+    print("spawning " .. id)
+    table.insert(nk.main.world, nk.main.entities[id]:new(args))
+    nk.main.world[#nk.main.world].index = #nk.main.world
+    local ent = nk.main.world[#nk.main.world]
+    nk.call("nk:entitySpawned", ent)
+    
+    if ret == true then
+        return ent
+    end
+end
+]]
--- builtin_mods/ui/ui_class.lua
+++ builtin_mods/ui/ui_class.lua
@@ -1,21 +1,62 @@
-nkui = class()
+function nk.ui.defineUI(id, eType)
+    local nkui = class(nk.main.basic_entity)
 
-function nkui:init(args)
-    for k, v in pairs(args) do
-        self[k] = v
+    function nkui:init(args)
+        for k, v in pairs(eType) do
+            self[k] = v
+        end
+        for k, v in pairs(args) do
+            self[k] = v
+        end
+        self.width = self.width or self.defaultWidth
+        self.height = self.height or self.defaultHeight
+        self.renderLayer = self.renderLayer or 100
     end
-    self.width = self.width or self.defaultWidth
-    self.height = self.height or self.defaultHeight
-    self.renderLayer = self.renderLayer or 100
-end
 
-function nkui:draw()
-    if self.x and self.y and self.width and self.height then
-        if self.image == nil then
-            nk.render(self.renderLayer, function ()
-                love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-            end)
+    function nkui:draw()
+        if self.x and self.y and self.width and self.height then
+            if self.image == nil then
+                nk.render(self.renderLayer, function ()
+                    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+                end)
+            end
         end
     end
+    table.insert(nk.main.uiClasses[id], nkui)
 end
 
+--[[
+    local entityClass = class(nk.main.basic_entity)
+    function entityClass:init(args)
+        for k, v in pairs(eType) do
+            self[k] = v
+        end
+        for k, v in pairs(args) do
+            self[k] = v
+        end
+        self.width = self.width or self.defaultWidth
+        self.height = self.height or self.defaultHeight
+
+        if self.width and self.height == nil and self.image then
+            local img = nk.getImage(self.image)
+            if img then
+                self.width = img:getWidth()
+                self.height = img:getHeight()
+            end
+        end
+
+        if self.onLoad then
+            self:onLoad()
+        end
+
+        -- if eType.collisionTag == nil then
+        --     eType.collisionTag = {env=true}
+        -- end
+    end
+
+    function entityClass:update()
+        if self.onUpdate then
+            self:onUpdate()
+        end
+    end
+]]
